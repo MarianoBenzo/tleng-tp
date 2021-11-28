e847625ffa266227d34fa2771a0d9936705e3d15
Juan Lanuza <juan.lanuza3@gmail.com>
2021-11-28 19:10:31 -0300

    Formateo gramatica.py

--- gramatica.py
+++ gramatica.py
@@ -1,5 +1,4 @@
-
-tokens  = (
+tokens = (
     'PARENTESIS_IZQ',
     'PARENTESIS_DER',
     'LLAVE_IZQ',
@@ -15,31 +14,38 @@ tokens  = (
     'TEXTO'
 )
 
+
 # Tokens
 
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += t.value.count("\n")
 
+
 def t_DESCRIPTOR(t):
     r'\[(.+)\s"(.+)"\]'
     return t
+
 
 def t_PARENTESIS_IZQ(t):
     r'\('
     return t
 
+
 def t_PARENTESIS_DER(t):
     r'\)'
     return t
+
 
 def t_LLAVE_IZQ(t):
     r'\{'
     return t
 
+
 def t_LLAVE_DER(t):
     r'\}'
     return t
+
 
 def t_NUM_JUGADAS_NEGRAS(t):
     r'\d+\.\.\.'
@@ -50,6 +56,7 @@ def t_NUM_JUGADAS_NEGRAS(t):
         t.value = 0
     return t
 
+
 def t_NUM_JUGADAS_BLANCAS(t):
     r'\d+\.'
     try:
@@ -59,37 +66,46 @@ def t_NUM_JUGADAS_BLANCAS(t):
         t.value = 0
     return t
 
+
 def t_MOVIMIENTO(t):
     r'([PNBRQK]?[a-h]?[1-8]?x?[a-h][1-8](\+\+|\+)?|O-O-O|O-O)(\!|\?)?'
     t.value = Movimiento('x' in t.value)
     return t
 
+
 def t_GANA_BLANCAS(t):
     r'1-0'
     return t
+
 
 def t_GANA_NEGRAS(t):
     r'0-1'
     return t
 
+
 def t_TABLAS(t):
     r'1/2-1/2'
     return t
+
 
 def t_ESPACIO(t):
     r'\s+'
     return t
 
+
 def t_TEXTO(t):
     r'[^\s{}()]+'
     return t
 
+
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
-    
+
+
 # Construyendo el analizador léxico
 import ply.lex as lex
+
 lexer = lex.lex()
 
 
@@ -98,13 +114,16 @@ class S:
     def __init__(self, nivel_maximo_sin_capturas):
         self.nivel_maximo_sin_capturas = nivel_maximo_sin_capturas
 
+
 class Partidas:
     def __init__(self, nivel_maximo_sin_capturas):
         self.nivel_maximo_sin_capturas = nivel_maximo_sin_capturas
 
+
 class Partida:
     def __init__(self, nivel_maximo_sin_capturas):
         self.nivel_maximo_sin_capturas = nivel_maximo_sin_capturas
+
 
 class Turnos:
     def __init__(self, color_primer_turno, numero_jugada_primer_turno, nivel_maximo_sin_capturas):
@@ -112,24 +131,29 @@ class Turnos:
         self.numero_jugada_primer_turno = numero_jugada_primer_turno
         self.nivel_maximo_sin_capturas = nivel_maximo_sin_capturas
 
+
 class Jugada:
     def __init__(self, color, numero_jugada, nivel_maximo_sin_capturas):
         self.color = color
         self.numero_jugada = numero_jugada
         self.nivel_maximo_sin_capturas = nivel_maximo_sin_capturas
 
+
 class InfoJugada:
     def __init__(self, nivel_maximo_sin_capturas):
         self.nivel_maximo_sin_capturas = nivel_maximo_sin_capturas
+
 
 class ComentarioContenidos:
     def __init__(self, nivel_maximo_sin_capturas, hay_capturas):
         self.nivel_maximo_sin_capturas = nivel_maximo_sin_capturas
         self.hay_capturas = hay_capturas
 
+
 class Comentario:
     def __init__(self, nivel_maximo_sin_capturas):
         self.nivel_maximo_sin_capturas = nivel_maximo_sin_capturas
+
 
 class Movimiento:
     def __init__(self, es_captura):
@@ -141,11 +165,14 @@ def p_s(t):
     's : partidas'
     print('\nMáximo nivel sin capturas: %d.' % int(t[1].nivel_maximo_sin_capturas))
 
+
 def p_partidas(t):
     '''partidas : partida partidas
                 | partida '''
-    nivel_maximo_sin_capturas = max(t[1].nivel_maximo_sin_capturas, t[2].nivel_maximo_sin_capturas) if len(t) == 3 else t[1].nivel_maximo_sin_capturas
+    nivel_maximo_sin_capturas = max(t[1].nivel_maximo_sin_capturas, t[2].nivel_maximo_sin_capturas) if len(t) == 3 \
+        else t[1].nivel_maximo_sin_capturas
     t[0] = Partidas(nivel_maximo_sin_capturas)
+
 
 def p_partida(t):
     'partida : descriptores turnos'
@@ -153,9 +180,11 @@ def p_partida(t):
     if t[2].numero_jugada_primer_turno != 1:
         print('El número de jugada debe comenzar en 1.')
 
+
 def p_descriptores(t):
     '''descriptores : DESCRIPTOR descriptores
                     | DESCRIPTOR'''
+
 
 def p_turnos(t):
     '''turnos   : jugada_blancas turnos_aux
@@ -165,41 +194,48 @@ def p_turnos(t):
         nivel_maximo_sin_capturas = max(t[1].nivel_maximo_sin_capturas, t[2].nivel_maximo_sin_capturas)
         t[0] = Turnos('Blancas', t[1].numero_jugada, nivel_maximo_sin_capturas)
 
-        if t[2].numero_jugada_primer_turno != None:
+        if t[2].numero_jugada_primer_turno is not None:
             if t[2].color_primer_turno == 'Negras' and t[2].numero_jugada_primer_turno != t[1].numero_jugada:
-                print('Los números de jugada %d y %d deben ser el mismo' % (t[1].numero_jugada, t[2].numero_jugada_primer_turno))
-            
+                print('Los números de jugada %d y %d deben ser el mismo' % (
+                    t[1].numero_jugada, t[2].numero_jugada_primer_turno))
+
             if t[2].color_primer_turno == 'Blancas' and t[2].numero_jugada_primer_turno != t[1].numero_jugada + 1:
                 if t[2].numero_jugada_primer_turno > t[1].numero_jugada + 1:
                     print('Faltan jugadas entre %d y %d.' % (t[1].numero_jugada, t[2].numero_jugada_primer_turno))
                 else:
-                    print('Las jugadas %d y %d no son crecientes.' % (t[1].numero_jugada, t[2].numero_jugada_primer_turno))
+                    print('Las jugadas %d y %d no son crecientes.' % (
+                        t[1].numero_jugada, t[2].numero_jugada_primer_turno))
     else:
         t[0] = Turnos(None, None, 0)
+
 
 def p_turnos_aux(t):
     '''turnos_aux   : jugada_negras turnos
                     | resultado'''
     if len(t) == 3:
         nivel_maximo_sin_capturas = max(t[1].nivel_maximo_sin_capturas, t[2].nivel_maximo_sin_capturas)
-        color_primer_turno = 'Negras' if t[1].numero_jugada != None else t[2].color_primer_turno
-        numero_jugada_primer_turno = t[1].numero_jugada if t[1].numero_jugada != None else t[2].numero_jugada_primer_turno
+        color_primer_turno = 'Negras' if t[1].numero_jugada is not None else t[2].color_primer_turno
+        numero_jugada_primer_turno = t[1].numero_jugada if t[1].numero_jugada is not None else t[
+            2].numero_jugada_primer_turno
 
         t[0] = Turnos(color_primer_turno, numero_jugada_primer_turno, nivel_maximo_sin_capturas)
 
-        if (t[1].numero_jugada != None and t[1].numero_jugada + 1 != t[2].numero_jugada_primer_turno):
+        if t[1].numero_jugada is not None and t[1].numero_jugada + 1 != t[2].numero_jugada_primer_turno:
             print('Las jugadas %d y %d no son consecutivas.' % (t[1].numero_jugada, t[2].numero_jugada_primer_turno))
     else:
         t[0] = Turnos(None, None, 0)
+
 
 def p_resultado(t):
     '''resultado    : GANA_BLANCAS
                     | GANA_NEGRAS
                     | TABLAS'''
 
+
 def p_jugada_blancas(t):
     'jugada_blancas : NUM_JUGADAS_BLANCAS ESPACIO info_jugada'
     t[0] = Jugada("Blancas", t[1], t[3].nivel_maximo_sin_capturas)
+
 
 def p_jugada_negras(t):
     '''jugada_negras    : NUM_JUGADAS_NEGRAS ESPACIO info_jugada 
@@ -209,19 +245,23 @@ def p_jugada_negras(t):
     else:
         t[0] = Jugada("Negras", None, t[1].nivel_maximo_sin_capturas)
 
+
 def p_info_jugada(t):
     'info_jugada : MOVIMIENTO ESPACIO info_jugada_aux'
     t[0] = t[3]
 
+
 def p_info_jugada_aux(t):
     '''info_jugada_aux  : comentario ESPACIO
-                        | empty''' 
+                        | empty'''
     t[0] = InfoJugada(t[1].nivel_maximo_sin_capturas) if len(t) == 3 else InfoJugada(0)
+
 
 def p_comentario(t):
     '''comentario   : LLAVE_IZQ comentario_contenidos LLAVE_DER
                     | PARENTESIS_IZQ comentario_contenidos PARENTESIS_DER'''
-    nivel_maximo_sin_capturas = 0 if t[2].nivel_maximo_sin_capturas == 0 and t[2].hay_capturas else t[2].nivel_maximo_sin_capturas + 1
+    nivel_maximo_sin_capturas = 0 if t[2].nivel_maximo_sin_capturas == 0 and t[2].hay_capturas\
+        else t[2].nivel_maximo_sin_capturas + 1
     t[0] = Comentario(nivel_maximo_sin_capturas)
 
 
@@ -244,6 +284,7 @@ def p_comentario_contenidos(t):
         else:
             t[0] = ComentarioContenidos(0, False)
 
+
 def p_comentario_contenido(t):
     '''comentario_contenido : comentario
                             | MOVIMIENTO
@@ -255,19 +296,21 @@ def p_comentario_contenido(t):
                             | TABLAS'''
     t[0] = t[1]
 
+
 def p_empty(p):
-     'empty :'
-     pass
+    'empty :'
+    pass
+
 
 def p_error(t):
-    print("Error sintáctico en '%s'" % t.value)
+    print(f"Error sintáctico en '{t.value}', linea {t.lineno}")
 
 
 import ply.yacc as yacc
+
 parser = yacc.yacc()
 
-
 f = open("./entrada.txt", "r")
-input = f.read()
-#print(input)
-parser.parse(input)
+input_file = f.read()
+# print(input_file)
+parser.parse(input_file)
